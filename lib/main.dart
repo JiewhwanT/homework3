de918c42e6cbfe6ee7d1933bd35f87278a761a5b
Tidarat Jiewhwan <jiewhwan_t@silpakorn.edu>
2022-09-22 21:58:46 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'guess_number/guess_number_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Homework2',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      home: const MyHomePage(title: 'GUESS THE NUMBER'),
+    );
+  }
+}
+
